1b3071e8ad9d771267b3c03fd8a5e0eed5104dda
Gurvinder211 <gsm21102004@gmail.com>
2026-02-01 13:32:46 +0530

    auto commit 2026-02-01

---
+++ backend/models/productModel.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+const prodictSchema = new mongoose.Schema({
+    
+})
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+ import express from 'express';
+ import cors from 'cors';
+ import bodyParser from 'body-parser';
+ import mongoose from 'mongoose';
+ import 'dotenv/config';
+ import connectDB from './config/mongodb.js';   
+import connectCloudinary from './config/cloudinary.js';
+
+ // App Config
+ const app = express();
+ const port = process.env.PORT || 4000;
+ connectDB();
+ connectCloudinary();
+
+
+ //Middleware
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+
+app.get('/', (req, res) => {
+   res.send("API Working");
+}) 
+
+app.listen(port, () => console.log('Server started on PORT: ' + port))
+ 
