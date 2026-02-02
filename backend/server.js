329e8d18a53af05732002b40515bcbe551619e70
Gurvinder211 <gsm21102004@gmail.com>
2026-02-02 11:42:29 +0530

    product adding feature done

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@
  import connectDB from './config/mongodb.js';   
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 
  // App Config
  const app = express();
@@ -21,10 +22,13 @@ app.use(cors());
 // api endpoints
 
 app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
 
 app.get('/', (req, res) => {
    res.send("API Working");
 }) 
+
+
 
 app.listen(port, () => console.log('Server started on PORT: ' + port))
  
