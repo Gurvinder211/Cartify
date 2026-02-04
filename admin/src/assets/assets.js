4657acf3adbecb4085ddb492480c348430aadafb
Gurvinder211 <gsm21102004@gmail.com>
2026-02-04 11:44:19 +0530

    created Navbar and Sidebar components

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.png'
+import logo from './logo.jpeg'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
@@ -7,7 +7,7 @@ import parcel_icon from './parcel_icon.svg'
 export const assets = {
     logo,
     add_icon,
-    order_icon,
+    order_icon, 
     upload_area,
     parcel_icon
 }
